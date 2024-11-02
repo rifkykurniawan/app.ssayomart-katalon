bfab184c37a0055fe65a9204b241a7d8bb044eba
rifkykurniawan <rifkykurniawan@localhost>
2024-11-02 23:52:02 +0700

    update test cases

--- Test Suites/Login.ts
+++ Test Suites/Login.ts
@@ -11,4 +11,18 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>6df62014-6dc0-4098-8b77-23ba6063765f</testSuiteGuid>
+   <testCaseLink>
+      <guid>8e258baa-4f8e-49a8-b23d-8136a8f40a8f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/LG001 - Login with valid email and valid password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f2c37152-174c-45ad-a2fd-9400ede177aa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/LG002 - Login with valid email and invalid password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
