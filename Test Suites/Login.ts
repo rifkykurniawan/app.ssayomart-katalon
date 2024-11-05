fa22c0c008e69bc25c0e26cc6b33425ad76c32e8
rifkykurniawan <rifkykurniawan@localhost>
2024-11-05 15:07:03 +0700

    Update test cases and tes suites

--- Test Suites/Login.ts
+++ Test Suites/Login.ts
@@ -12,17 +12,31 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>6df62014-6dc0-4098-8b77-23ba6063765f</testSuiteGuid>
    <testCaseLink>
-      <guid>8e258baa-4f8e-49a8-b23d-8136a8f40a8f</guid>
+      <guid>da2d67b7-7766-4d3e-a7db-c7ad51c241f9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Login/LG001 - Login with valid email and valid password</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>f2c37152-174c-45ad-a2fd-9400ede177aa</guid>
+      <guid>bc390cf2-628a-4e7d-b41b-24ace4a20dbe</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Login/LG002 - Login with valid email and invalid password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1836ce62-2449-469b-b235-dab4b778730a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/LG004 - Login with invalid email and invalid password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1639f0c6-4c00-4d16-97cc-00dc2eae397e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/LG003 - Login with invalid email and valid password</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
